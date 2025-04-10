6f36770176e19ec643a560ed3e52e314db6eb996
Dan Vanderkam <danvdk@gmail.com>
2025-04-10 14:42:11 -0500

    fix layout in firefox (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SWRConfig } from "swr";
-import "./index.css";
 import App from "./App.tsx";
 import { singletonCache } from "./cache.ts";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
